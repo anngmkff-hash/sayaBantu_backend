c6ee6e11399e6919f552736aebac5ca9d9f945e4
anngmkff-hash <anang.makruf241206@gmail.com>
2025-12-31 14:18:16 +0700

    prepare backend for deployment

--- server.js
+++ server.js
@@ -53,11 +53,11 @@ async function sendResetEmail(to, link) {
 // DB (pakai .env)
 // ============================
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "",
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
+  port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
   connectionLimit: 10,
 });
 
